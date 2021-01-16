7f15669f6536608210583c40cbc1f104452fc784
Neles Rodrigues <neles.rodrigues@arce.ce.gov.br>
2021-01-16 14:37:42 -0300

    correções

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -7,6 +7,6 @@ class Endereco < ApplicationRecord
     self.logradouro = "Sem Informação" if self.logradouro.blank?
     self.bairro = "Sem Informação" if self.bairro.blank?
     self.endereco_completo = "#{self.logradouro} - #{self.bairro} - #{self.cidade}/#{self.uf} - #{self.cep}"
-      #self.cep = self.cep ? self.cep.gsub(/\D+/, "").squeeze(" ").strip : self.cep
+    self.cep = self.cep.gsub(/\D+/, "").squeeze(" ").strip
   end
 end
